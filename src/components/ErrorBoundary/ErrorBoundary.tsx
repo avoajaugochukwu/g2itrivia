bc476bed229a6f5cfde7efec14de734a708231d9
Ugochukwu Charles <karlthegoo@hotmail.com>
2022-04-05 16:10:40 +0100

    Removed console.log

--- src/components/ErrorBoundary/ErrorBoundary.tsx
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -21,7 +21,7 @@ class ErrorBoundary extends Component<Props, State> {
   }
 
   public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
-    console.error('Uncaught error:', error, errorInfo);
+    return `Uncaught error:', ${error}, ${errorInfo}`;
   }
 
   public render() {
